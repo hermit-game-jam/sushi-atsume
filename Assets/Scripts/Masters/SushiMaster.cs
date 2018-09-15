8fc4977a804cbe885e40084b04062387da03bea3
kanais_7 <kanais0620@gmail.com>
2018-09-16 04:39:15 +0900

    Add SushiMaster

---
+++ Assets/Scripts/Masters/SushiMaster.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Masters
+{
+    [Serializable]
+    public class SushiMaster
+    {
+        public int Code;
+        public Price Price;
+    }
+}
---
+++ Assets/Scripts/Masters/SushiMasterRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Masters
+{
+    [CreateAssetMenu(menuName = "Assets/Create SushiMasterRepository")]
+    public class SushiMasterRepository : ScriptableObject
+    {
+        public List<SushiMaster> Values;
+    }
+}

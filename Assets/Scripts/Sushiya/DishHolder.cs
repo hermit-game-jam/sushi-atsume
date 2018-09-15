11f69e417cd4b394e53da52e5232a4148cd29c0a
fumi <fumilin01@gmail.com>
2018-09-16 03:17:18 +0900

    DishHolderにAddとRemove関数追加

--- Assets/Scripts/Sushiya/DishHolder.cs
+++ Assets/Scripts/Sushiya/DishHolder.cs
@@ -1,11 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
 namespace Sushiya
 {
     public class DishHolder
     {
+        private readonly Queue<int> dishQueue = new Queue<int>();
+
+        /// <param name="sushiCode"></param>
+        public void Add(int sushiCode)
+        {
+            dishQueue.Enqueue(sushiCode);
+            // TODO: 追加された時に何かのイベント発火する
+        }
         
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num">減らす個数</param>
+        /// <returns>減った時の寿司コード</returns>
+        public IEnumerable<int> Remove(int num)
+        {
+            for (var i = 0; i < num; i++)
+            {
+                yield return dishQueue.Dequeue();
+            }
+        }
     }
 }

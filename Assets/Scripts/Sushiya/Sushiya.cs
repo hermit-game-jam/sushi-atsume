b17c0bb3cebb6468a68ffd0e2ffb72eb7bcd00dc
fumi <fumilin01@gmail.com>
2018-09-16 03:03:20 +0900

    ネームスペースsushiya付けた

--- Assets/Scripts/Sushiya/Sushiya.cs
+++ Assets/Scripts/Sushiya/Sushiya.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sushiya : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Sushiya
+{
+    public class Sushiya : MonoBehaviour
+    {
+    
+    }
 }

b887ae7d09c23a843f9e818a4b6754a675434699
Shoma SATO <noir.neo.04@gmail.com>
2018-09-16 04:40:05 +0900

    Move sushi!!!

---
+++ Assets/Scripts/Sushi/SushiCore.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Sushi
+{
+    public class SushiCore : MonoBehaviour, IClickable
+    {
+        ISushiState state;
+        public ISushiState State => state;
+
+        void Awake()
+        {
+            ChangeState(new LaneSushiState(this));
+        }
+
+        public void ChangeState(ISushiState state)
+        {
+            this.state = state;
+        }
+
+        void IClickable.OnClick()
+        {
+            state.OnClick();
+        }
+
+        class LaneSushiState : ISushiState
+        {
+            readonly SushiCore core;
+            public bool AutoMovable => true;
+
+            public LaneSushiState(SushiCore core)
+            {
+                this.core = core;
+            }
+
+            void ISushiState.OnClick()
+            {
+                core.ChangeState(new TableSushiState(core));
+            }
+        }
+
+        class TableSushiState : ISushiState
+        {
+            readonly SushiCore core;
+            public bool AutoMovable => false;
+
+            public TableSushiState(SushiCore core)
+            {
+                this.core = core;
+            }
+            void ISushiState.OnClick()
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Sushi/SushiMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Sushi
+{
+    public class SushiMover : MonoBehaviour
+    {
+        [SerializeField] SushiCore core;
+        [SerializeField] float speed;
+
+        void Update()
+        {
+            if (core.State.AutoMovable)
+            {
+                transform.position = transform.position + Vector3.left * speed * Time.deltaTime;
+            }
+        }
+    }
+}

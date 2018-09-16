c6b31f537319bc8af0e14bfe13221d123da010a4
kanais_7 <kanais0620@gmail.com>
2018-09-16 13:03:32 +0900

    Fix Gacha Draw Trigger

--- Assets/Scripts/Gachas/GachaDrawer.cs
+++ Assets/Scripts/Gachas/GachaDrawer.cs
@@ -5,11 +5,15 @@ namespace Gachas
 {
     public class GachaDrawer
     {
+        public static int DrawCost = 5;
+
         public GachaDrawer(DishHolder dishHolder)
         {
             dishHolder.AddDishObservable
+                .Where(_ => dishHolder.CurrentDishCount == DrawCost)
                 .Subscribe(_ =>
                 {
+                    dishHolder.Remove(DrawCost);
                     // ガチャ引く
                 });
         }

17f90faece01746eefcdb6ab7a166991513c2a98
fumi <fumilin01@gmail.com>
2018-09-16 14:29:52 +0900

    寿司の皿をクリックした時に、GameObjectが破棄されて音がならない問題の修正

--- Assets/Scripts/Sushi/SushiEffecter.cs
+++ Assets/Scripts/Sushi/SushiEffecter.cs
@@ -29,18 +29,31 @@ namespace Sushi
             
             core.OnEmptySushiClick.Subscribe(_ =>
             {
-                PlaySound(dropDishClips);
+                PlaySound(dropDishClips, true);
             }).AddTo(this);
         }
 
-        void PlaySound(List<AudioClip> clips)
+        void PlaySound(List<AudioClip> clips, bool playOutSide = false)
         {
             var selectedClip = clips.Random();
             var pitch = Random.Range(0.95f, 1.05f);
+
+            var a = playOutSide ? CreateOutsideAudioSource(selectedClip) : audioSource;
             
-            audioSource.clip = selectedClip;
-            audioSource.pitch = pitch;
-            audioSource.Play();
+            a.clip = selectedClip;
+            a.pitch = pitch;
+            a.Play();
+        }
+        
+        // AudioSource
+        // public static void PlayClipAtPoint(AudioClip clip, Vector3 position, [DefaultValue("1.0F")] float volume)
+        // より拝借
+        AudioSource CreateOutsideAudioSource(AudioClip clip)
+        {
+            var go = new GameObject("One shot audio");
+            var a = go.AddComponent<AudioSource>();
+            Destroy(go, clip.length * ((double) Time.timeScale >= 0.00999999977648258 ? Time.timeScale : 0.01f));
+            return a;
         }
     }
 }

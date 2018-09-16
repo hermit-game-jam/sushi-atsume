8bedc3778487090b97073e38dab2530f9b592c2b
kanais_7 <kanais0620@gmail.com>
2018-09-16 14:25:29 +0900

    Add Denpyo Base

---
+++ Assets/Scripts/Sushiya/Denpyo.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Masters;
+
+namespace Sushiya
+{
+    public class Denpyo
+    {
+        private readonly Dictionary<int, int> sushiCountPairs = Master.Instance.SushiMaster.Values.ToDictionary(x => x.Code, _ => 0);
+        
+        public void Add(int sushiCode)
+        {
+            sushiCountPairs[sushiCode]++;
+        }
+
+        public int Sum()
+        {
+            return sushiCountPairs
+                .Select(x => Master.Instance.SushiMaster.Values[x.Key].Price.Value * x.Value)
+                .Sum();
+        }
+    }
+}
--- Assets/Scripts/Sushiya/Sushiya.cs
+++ Assets/Scripts/Sushiya/Sushiya.cs
@@ -9,11 +9,13 @@ namespace Sushiya
     {   
         public DishHolder DishHolder = new DishHolder();
         private SushiMenu SushiMenu;
+        private Denpyo Denpyo;
         private GachaDrawer GachaDrawer;
 
         void Start()
         {
             SushiMenu = new SushiMenu();
+            Denpyo = new Denpyo();
             GachaDrawer = new GachaDrawer(DishHolder); 
         }
     }

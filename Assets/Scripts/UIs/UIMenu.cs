0c72dad7038b95c4a790535f2cc1e90c730d1e86
Shoma SATO <noir.neo.04@gmail.com>
2018-09-16 19:51:44 +0900

    Menu Text

--- Assets/Scripts/UIs/UIMenu.cs
+++ Assets/Scripts/UIs/UIMenu.cs
@@ -7,21 +7,35 @@ namespace UIs
     public class UIMenu : MonoBehaviour
     {
         [SerializeField] SushiHolder holder;
+        [SerializeField] Quaternion sushiAngle;
+        [SerializeField] MenuTexts textPrefab;
 
         void Start()
         {
             foreach (var master in Master.Instance.SushiMaster.Values)
             {
-                var sushi = Create(master);
-                holder.TryPut(sushi.transform);
+                var sushi = CreateSushi(master);
+                if (holder.TryPut(sushi.transform))
+                {
+                    sushi.transform.rotation = sushiAngle;
+                    var text = CreateText(master);
+                    text.transform.SetParent(sushi.transform.parent, false);
+                }
             }
         }
 
-        GameObject Create(SushiMaster master)
+        GameObject CreateSushi(SushiMaster master)
         {
             var sushi = master.SushiCreate(Vector3.zero, Quaternion.identity, transform);
             sushi.ChangeStateToMenu();
             return sushi.gameObject;
+        }
+
+        GameObject CreateText(SushiMaster master)
+        {
+            var text = Instantiate(textPrefab);
+            text.Configure(master);
+            return text.gameObject;
         }
     }
 }

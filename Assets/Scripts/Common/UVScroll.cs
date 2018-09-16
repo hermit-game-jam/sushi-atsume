2e94777c1e56177164d6be599e7041fed8f8c096
fumi <fumilin01@gmail.com>
2018-09-16 20:31:04 +0900

    レーン追加/寿司の速度と沸く速度調整

---
+++ Assets/Scripts/Common/UVScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVScroll : MonoBehaviour
+{
+    public Vector2 speed;
+    public string propertyName;
+
+    private Material mat;
+    private int propertyId;
+    
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        propertyId = Shader.PropertyToID(propertyName);
+    }
+    
+    void Update()
+    {
+        var current =  mat.GetTextureOffset(propertyId);
+        var next = current + Time.deltaTime * speed;
+        next = new Vector2(next.x % 1, next.y % 1);
+        mat.SetTextureOffset(propertyId, next);
+    }
+}

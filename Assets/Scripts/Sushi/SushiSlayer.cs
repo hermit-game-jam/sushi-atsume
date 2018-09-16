1f717154c6f6d224d1903f1b46ad387de68cd2a0
Shoma SATO <noir.neo.04@gmail.com>
2018-09-16 11:32:03 +0900

    Create and Destroy Sushi

---
+++ Assets/Scripts/Sushi/SushiSlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Sushi
+{
+    public class SushiSlayer : MonoBehaviour
+    {
+        [SerializeField] float lifetimeSeconds;
+
+        void Start()
+        {
+            Observable.Timer(TimeSpan.FromSeconds(lifetimeSeconds))
+                .Subscribe(_ => Destroy(gameObject))
+                .AddTo(this);
+        }
+    }
+}

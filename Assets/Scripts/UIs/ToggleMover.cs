2b7a7a23a4e307670be357a17820e8031d7a1605
Shoma SATO <noir.neo.04@gmail.com>
2018-09-16 18:42:14 +0900

    Zoom menu

---
+++ Assets/Scripts/UIs/ToggleMover.cs
@@ -0,0 +1,25 @@
+﻿using Sushi;
+using UniRx;
+using UnityEngine;
+
+namespace UIs
+{
+    public class ToggleMover : MonoBehaviour, IClickable
+    {
+        readonly BoolReactiveProperty zooming = new BoolReactiveProperty();
+        [SerializeField] Animator _animator;
+
+        void Start()
+        {
+            zooming.Subscribe(x => _animator.SetBool("zooming", x))
+                .AddTo(this);
+        }
+
+        void IClickable.OnClick()
+        {
+            zooming.Value = !zooming.Value;
+        }
+
+
+    }
+}
